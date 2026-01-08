3488927f330d52e75b6a9c8cafefafa7a4d785a5
boli2257 <fintaboli2257@gmail.com>
2026-01-08 10:39:40 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { AddTask } from './components/AddTaSK.JSX'
+
+function App() {
+
+
+  return (
+    <>
+    Csapat projekt!
+    <AddTask/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AddTask = () => {
+  return (
+    <div>
+      <h1>Új feladat hozzáadása</h1>
+      <h3>asd</h3>
+    </div>
+  )
+}
+
